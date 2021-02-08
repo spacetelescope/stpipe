7af5b421b893513302da35481bb3f63efb053790
Ed Slavich <eslavich@stsci.edu>
2021-02-08 10:28:42 -0500

    Define an StpipeExitException to avoid potential problems caused by raising SystemExit

--- src/stpipe/exceptions.py
+++ src/stpipe/exceptions.py
@@ -3,3 +3,17 @@ class StpipeException(Exception):
     Base class for exceptions from the stpipe package.
     """
     pass
+
+
+class StpipeExitException(StpipeException):
+    """
+    An exception that carries an exit status that is
+    returned by stpipe CLI tools.
+    """
+    def __init__(self, exit_status, *args):
+        super().__init__(exit_status, *args)
+        self._exit_status = exit_status
+
+    @property
+    def exit_status(self):
+        return self._exit_status
