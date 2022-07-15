3cab4bcc74a9b04b8fcd99b276e4b6e7e94c4dfb
Nadia Dencheva <nadia.dencheva@gmail.com>
2022-07-14 21:40:09 -0500

    pass crds_parameters, not a model

--- src/stpipe/pipeline.py
+++ src/stpipe/pipeline.py
@@ -182,24 +182,22 @@ class Pipeline(Step):
         with cls._datamodels_open(dataset, asn_n_members=1) as model:
             if isinstance(model, Sequence):
                 for contained_model in model:
-                    input_class = contained_model.__class__()
-                    metadata = input_class
-                    metadata.update(contained_model, only='PRIMARY')
+                    crds_parameters = model.get_crds_parameters()
+                    crds_observatory = model.crds_observatory
             else:
-                input_class = model.__class__()
-                metadata = input_class
-                metadata.update(model, only='PRIMARY')
+                crds_parameters = model.get_crds_parameters()
+                crds_observatory = model.crds_observatory
 
         for cal_step in cls.step_defs.keys():
             cal_step_class = cls.step_defs[cal_step]
-            refcfg['steps'][cal_step] = cal_step_class.get_config_from_reference(metadata)
+            refcfg['steps'][cal_step] = cal_step_class.get_config_from_reference(model)
         #
         # Now merge any config parameters from the step cfg file
         logger.debug(f'Retrieving pipeline {reftype.upper()} parameters from CRDS')
         try:
-            ref_file = crds_client.get_reference_file(metadata.get_crds_parameters(),
-                                                    reftype,
-                                                    metadata.crds_observatory)
+            ref_file = crds_client.get_reference_file(crds_parameters,
+                                                      reftype,
+                                                      crds_observatory)
         except (AttributeError, crds_client.CrdsError):
             logger.debug(f'{reftype.upper()}: No parameters found')
         else:
