32e8df3487f7387b327c40a119f5a8c35f15454d
James Davies <jdavies@mpia.de>
2023-08-23 11:45:54 +0200

    Move strun script to entrypoints

--- src/stpipe/cli/strun.py
+++ src/stpipe/cli/strun.py
@@ -1,29 +1,27 @@
-#!/usr/bin/env python
-
-"""
-Run Steps from the command line
-
-Exit Status
------------
-    0:  Step completed satisfactorily
-    1:  General error occurred
-
-Other status codes are Step implementation-specific.
-"""
-
 import sys
 
 from stpipe import Step
 from stpipe.cli.main import _print_versions
 from stpipe.exceptions import StpipeExitException
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Run Steps from the command line
+
+    Exit Status
+    -----------
+        0:  Step completed satisfactorily
+        1:  General error occurred
+
+    Other status codes are Step implementation-specific.
+    """
     if "--version" in sys.argv:
         _print_versions()
         sys.exit(0)
 
     try:
-        step = Step.from_cmdline(sys.argv[1:])
+        Step.from_cmdline(sys.argv[1:])
     except StpipeExitException as e:
         sys.exit(e.exit_status)
     except Exception:
