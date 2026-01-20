f72ccfe0c06a1ffa91e6f67bf9e53fdd3bb2162d
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-01-20 18:18:13 -0500

    Do not depend on save_results

--- src/stpipe/step.py
+++ src/stpipe/step.py
@@ -523,7 +523,7 @@ class Step:
             if self.output_file is not None:
                 self.save_results = True
 
-            if self.save_results and self.output_dir:
+            if self.output_dir:
                 os.makedirs(self.output_dir, exist_ok=True)
 
             if self.suffix is None:
