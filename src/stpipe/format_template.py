784713deb0306b7caa453dfefefe4068aedea3be
Brett Graham <brettgraham@gmail.com>
2026-08-20 11:44:25 -0400

    Switch fstrings to f-strings
    Co-authored-by: Melanie Clarke <mclarke@stsci.edu>

--- src/stpipe/format_template.py
+++ src/stpipe/format_template.py
@@ -3,7 +3,7 @@ import warnings
 from ._format_template import *  # noqa: F403
 
 warnings.warn(
-    "stpipe.format_template is deprecated. Use fstrings instead.",
+    "stpipe.format_template is deprecated. Use f-strings instead.",
     DeprecationWarning,
     stacklevel=2,
 )
